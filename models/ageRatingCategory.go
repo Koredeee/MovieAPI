7bc1ec8b5d8b1449506a50564a158717a9d36b10
Koredeee <m.fatihdaffa@gmail.com>
2023-11-22 21:52:47 +0700

    Added Models

--- models/ageRatingCategory.go
+++ models/ageRatingCategory.go
@@ -1,1 +1,14 @@
 package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type (
+	AgeRatingCategory struct {
+		gorm.Model
+		Name        string  `json:"name"`
+		Description string  `json:"description"`
+		Movies      []Movie `json:"-"`
+	}
+)
--- models/movie.go
+++ models/movie.go
@@ -1,1 +1,13 @@
 package models
+
+import "gorm.io/gorm"
+
+type (
+	Movie struct {
+		gorm.Model
+		Title               string            `json:"title"`
+		Year                int               `json:"year"`
+		AgeRatingCategoryID uint              `json:"ageRatingCategoryID"`
+		AgeRatingCategory   AgeRatingCategory `json:"-"`
+	}
+)
