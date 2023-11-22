8408a73f903a731b616230fd6e4da2529201c1eb
Koredeee <m.fatihdaffa@gmail.com>
2023-11-23 00:03:21 +0700

    Added routes and Updated main

--- docs/docs.go
+++ docs/docs.go
@@ -9,12 +9,259 @@ const docTemplate = `{
     "info": {
         "description": "{{escape .Description}}",
         "title": "{{.Title}}",
-        "contact": {},
+        "termsOfService": "http://swagger.io/terms/",
+        "contact": {
+            "name": "API Support",
+            "url": "http://www.swagger.io/support",
+            "email": "support@swagger.io"
+        },
+        "license": {
+            "name": "Apache 2.0",
+            "url": "http://www.apache.org/licenses/LICENSE-2.0.html"
+        },
         "version": "{{.Version}}"
     },
     "host": "{{.Host}}",
     "basePath": "{{.BasePath}}",
-    "paths": {}
+    "paths": {
+        "/age-rating-categories": {
+            "get": {
+                "description": "Get List of Age Rating Category",
+                "produces": [
+                    "application/json"
+                ],
+                "tags": [
+                    "AgeRatingCategory"
+                ],
+                "summary": "Get All Age Rating Category",
+                "responses": {
+                    "200": {
+                        "description": "OK",
+                        "schema": {
+                            "type": "array",
+                            "items": {
+                                "$ref": "#/definitions/models.AgeRatingCategory"
+                            }
+                        }
+                    }
+                }
+            },
+            "post": {
+                "description": "Create new Age Rating Category",
+                "produces": [
+                    "application/json"
+                ],
+                "tags": [
+                    "AgeRatingCategory"
+                ],
+                "summary": "Create Age Rating Category",
+                "parameters": [
+                    {
+                        "description": "the body to create new age rating category",
+                        "name": "Body",
+                        "in": "body",
+                        "required": true,
+                        "schema": {
+                            "$ref": "#/definitions/controllers.AgeRatingCategoryInput"
+                        }
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "OK",
+                        "schema": {
+                            "$ref": "#/definitions/models.AgeRatingCategory"
+                        }
+                    }
+                }
+            }
+        },
+        "/age-rating-categories/{id}": {
+            "get": {
+                "description": "Get one Age Rating category by Id",
+                "produces": [
+                    "application/json"
+                ],
+                "tags": [
+                    "AgeRatingCategory"
+                ],
+                "summary": "Get an Age Rating Category by Id",
+                "parameters": [
+                    {
+                        "type": "string",
+                        "description": "Age Eating Category Id",
+                        "name": "id",
+                        "in": "path",
+                        "required": true
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "OK",
+                        "schema": {
+                            "$ref": "#/definitions/models.AgeRatingCategory"
+                        }
+                    }
+                }
+            },
+            "delete": {
+                "description": "Delete one Age Rating category by Id",
+                "produces": [
+                    "application/json"
+                ],
+                "tags": [
+                    "AgeRatingCategory"
+                ],
+                "summary": "Delete an Age Rating Category by Id",
+                "parameters": [
+                    {
+                        "type": "string",
+                        "description": "Age Eating Category Id",
+                        "name": "id",
+                        "in": "path",
+                        "required": true
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "OK",
+                        "schema": {
+                            "type": "object",
+                            "additionalProperties": {
+                                "type": "boolean"
+                            }
+                        }
+                    }
+                }
+            },
+            "patch": {
+                "description": "Update one Age Rating category by Id",
+                "produces": [
+                    "application/json"
+                ],
+                "tags": [
+                    "AgeRatingCategory"
+                ],
+                "summary": "Update an Age Rating Category by Id",
+                "parameters": [
+                    {
+                        "type": "string",
+                        "description": "Age Eating Category Id",
+                        "name": "id",
+                        "in": "path",
+                        "required": true
+                    },
+                    {
+                        "description": "the body to create new age rating category",
+                        "name": "Body",
+                        "in": "body",
+                        "required": true,
+                        "schema": {
+                            "$ref": "#/definitions/controllers.AgeRatingCategoryInput"
+                        }
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "OK",
+                        "schema": {
+                            "$ref": "#/definitions/models.AgeRatingCategory"
+                        }
+                    }
+                }
+            }
+        },
+        "/age-rating-categories/{id}/movies": {
+            "get": {
+                "description": "Get all movies of Age Rating Category by id",
+                "produces": [
+                    "application/json"
+                ],
+                "tags": [
+                    "AgeRatingCategory"
+                ],
+                "summary": "Get movies by Age Rating Category by id",
+                "parameters": [
+                    {
+                        "type": "string",
+                        "description": "Age Eating Category Id",
+                        "name": "id",
+                        "in": "path",
+                        "required": true
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "OK",
+                        "schema": {
+                            "type": "array",
+                            "items": {
+                                "$ref": "#/definitions/models.Movie"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    },
+    "definitions": {
+        "controllers.AgeRatingCategoryInput": {
+            "type": "object",
+            "properties": {
+                "description": {
+                    "type": "string"
+                },
+                "name": {
+                    "type": "string"
+                }
+            }
+        },
+        "models.AgeRatingCategory": {
+            "type": "object",
+            "properties": {
+                "created_at": {
+                    "type": "string"
+                },
+                "description": {
+                    "type": "string"
+                },
+                "id": {
+                    "description": "gorm.Model",
+                    "type": "integer"
+                },
+                "name": {
+                    "type": "string"
+                },
+                "updated_at": {
+                    "type": "string"
+                }
+            }
+        },
+        "models.Movie": {
+            "type": "object",
+            "properties": {
+                "age_rating_category_id": {
+                    "type": "integer"
+                },
+                "created_at": {
+                    "type": "string"
+                },
+                "id": {
+                    "description": "gorm.Model",
+                    "type": "integer"
+                },
+                "title": {
+                    "type": "string"
+                },
+                "updated_at": {
+                    "type": "string"
+                },
+                "year": {
+                    "type": "integer"
+                }
+            }
+        }
+    }
 }`
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
---
+++ routes/routes.go
@@ -0,0 +1,31 @@
+package routes
+
+import (
+	"MovieAPI/controllers"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+
+	swaggerFiles "github.com/swaggo/files"     // swagger embed files
+	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
+)
+
+func SetupRouter(db *gorm.DB) *gin.Engine {
+	r := gin.Default()
+
+	// set db to gin context
+	r.Use(func(ctx *gin.Context) {
+		ctx.Set("db", db)
+	})
+
+	r.GET("/age-rating-categories", controllers.GetAllRating)
+	r.POST("/age-rating-categories", controllers.CreateRating)
+	r.GET("/age-rating-categories/:id", controllers.GetRatingById)
+	r.GET("/age-rating-categories/:id/movies", controllers.GetMoviesByAgeRatingCategoryId)
+	r.PATCH("/age-rating-categories/:id", controllers.UpdateRating)
+	r.DELETE("/age-rating-categories/:id", controllers.DeleteRating)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	return r
+}
